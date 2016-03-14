2d07dade4280682d4b932a35156681c77f92f07e
Nicholas Castaneda <npcastaneda@gmail.com>
2016-03-13 21:44:54 -0700

    added react practice

---
+++ reactPractice/index.ios.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
